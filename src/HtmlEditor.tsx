94a9c4c5d7fd6587ea43b9a881bbaa71b11a24a0
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-22 16:36:44 +0900

    ADD: react-quill 작업

---
+++ src/HtmlEditor.tsx
@@ -0,0 +1,59 @@
+// src/MyEditor.tsx
+import React, { useMemo, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css"; // CSS 임포트
+
+const formats = [
+  "font",
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "align",
+  "color",
+  "background",
+  "size",
+  "h1",
+];
+
+const HtmlEditor: React.FC = () => {
+  const [content, setContent] = useState<string>("");
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          ["link", "image"],
+          [{ size: ["small", false, "large", "huge"] }],
+          [{ align: [] }],
+          ["bold", "italic", "underline", "strike"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["clean"],
+          [
+            {
+              color: [],
+            },
+            { background: [] },
+          ],
+        ],
+      },
+    };
+  }, []);
+  const handleChange = (value: string) => {
+    setContent(value);
+  };
+
+  return (
+    <div>
+      <ReactQuill value={content} modules={modules} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default HtmlEditor;
