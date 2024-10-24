b2f335c8c117eb8f785e09323c9fc271845b7bc8
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-24 14:27:54 +0900

    Fix:

--- src/App.tsx
+++ src/App.tsx
@@ -81,8 +81,6 @@ export default function App() {
               },
             });
           },
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
       />
 
