f93d54454f8866f56d898e6094e826e7adb1d10d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-24 13:21:53 +0900

    ADD: license 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Editor as TinyMCEEditor } from "tinymce";
 import "./App.css";
 
 export default function App() {
   const editorRef = useRef<TinyMCEEditor | null>(null);
+  const [content, setContent] = useState<string>("");
   const log = () => {
     if (editorRef.current) {
-      console.log(editorRef.current.getContent());
+      setContent(editorRef.current.getContent());
     }
   };
   return (
     <>
       <Editor
+        onChange={(value) => console.log(value)}
         tinymceScriptSrc="/tinymce/tinymce.min.js"
-        licenseKey="your-license-key"
+        licenseKey="gpl"
         onInit={(_evt, editor) => (editorRef.current = editor)}
         initialValue="<p>This is the initial content of the editor.</p>"
         init={{
@@ -43,12 +45,22 @@ export default function App() {
             "undo redo | blocks | " +
             "bold italic forecolor | alignleft aligncenter " +
             "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help",
+            "removeformat | help | table",
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
       />
       <button onClick={log}>Log editor content</button>
+
+      <div
+        style={{
+          width: "100%",
+          border: "1px solid black",
+        }}
+        dangerouslySetInnerHTML={{
+          __html: content,
+        }}
+      ></div>
     </>
   );
 }
